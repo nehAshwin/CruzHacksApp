ab0f3c0fae7a9db6bf61e24bca48766d343d4ac4
Neha Ashwin <nashwin2548@gmail.com>
2024-01-20 14:26:47 -0800

    Added PopUpView

---
+++ CruzHacks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  CruzHacks
+//
+//  Created by Neha Ashwin on 1/20/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
